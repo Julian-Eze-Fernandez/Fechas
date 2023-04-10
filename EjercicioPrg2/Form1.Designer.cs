a08109be8bc9f927e8a5c6bb7e466813dedac7a5
Julian Fernandez <fernandezjulian00@hotmail.com>
2023-04-10 01:28:28 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioPrg2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace EjercicioPrg2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtAño = new System.Windows.Forms.TextBox();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.LblMes = new System.Windows.Forms.Label();
+            this.LblResultado = new System.Windows.Forms.Label();
+            this.LblAño = new System.Windows.Forms.Label();
+            this.LblDia = new System.Windows.Forms.Label();
+            this.CmbBoxMes = new System.Windows.Forms.ComboBox();
+            this.TxtDia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtAño
+            // 
+            this.TxtAño.Location = new System.Drawing.Point(131, 30);
+            this.TxtAño.MaxLength = 4;
+            this.TxtAño.Name = "TxtAño";
+            this.TxtAño.Size = new System.Drawing.Size(52, 20);
+            this.TxtAño.TabIndex = 2;
+            // 
+            // Btn1
+            // 
+            this.Btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn1.Location = new System.Drawing.Point(207, 9);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(77, 42);
+            this.Btn1.TabIndex = 3;
+            this.Btn1.Text = "Adivinar";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // LblMes
+            // 
+            this.LblMes.AutoSize = true;
+            this.LblMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMes.Location = new System.Drawing.Point(70, 9);
+            this.LblMes.Name = "LblMes";
+            this.LblMes.Size = new System.Drawing.Size(37, 17);
+            this.LblMes.TabIndex = 4;
+            this.LblMes.Text = "Mes";
+            // 
+            // LblResultado
+            // 
+            this.LblResultado.AutoSize = true;
+            this.LblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResultado.Location = new System.Drawing.Point(87, 118);
+            this.LblResultado.Name = "LblResultado";
+            this.LblResultado.Size = new System.Drawing.Size(0, 17);
+            this.LblResultado.TabIndex = 5;
+            // 
+            // LblAño
+            // 
+            this.LblAño.AutoSize = true;
+            this.LblAño.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAño.Location = new System.Drawing.Point(128, 9);
+            this.LblAño.Name = "LblAño";
+            this.LblAño.Size = new System.Drawing.Size(36, 17);
+            this.LblAño.TabIndex = 6;
+            this.LblAño.Text = "Año";
+            // 
+            // LblDia
+            // 
+            this.LblDia.AutoSize = true;
+            this.LblDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia.Location = new System.Drawing.Point(12, 9);
+            this.LblDia.Name = "LblDia";
+            this.LblDia.Size = new System.Drawing.Size(32, 17);
+            this.LblDia.TabIndex = 7;
+            this.LblDia.Text = "Dia";
+            // 
+            // CmbBoxMes
+            // 
+            this.CmbBoxMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CmbBoxMes.FormattingEnabled = true;
+            this.CmbBoxMes.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.CmbBoxMes.Location = new System.Drawing.Point(70, 30);
+            this.CmbBoxMes.Name = "CmbBoxMes";
+            this.CmbBoxMes.Size = new System.Drawing.Size(40, 21);
+            this.CmbBoxMes.TabIndex = 9;
+            // 
+            // TxtDia
+            // 
+            this.TxtDia.Location = new System.Drawing.Point(15, 31);
+            this.TxtDia.MaxLength = 2;
+            this.TxtDia.Name = "TxtDia";
+            this.TxtDia.Size = new System.Drawing.Size(40, 20);
+            this.TxtDia.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "El dia proximo sera:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(334, 203);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtDia);
+            this.Controls.Add(this.CmbBoxMes);
+            this.Controls.Add(this.LblDia);
+            this.Controls.Add(this.LblAño);
+            this.Controls.Add(this.LblResultado);
+            this.Controls.Add(this.LblMes);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.TxtAño);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ejercicio Fecha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TxtAño;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Label LblMes;
+        private System.Windows.Forms.Label LblResultado;
+        private System.Windows.Forms.Label LblAño;
+        private System.Windows.Forms.Label LblDia;
+        private System.Windows.Forms.ComboBox CmbBoxMes;
+        private System.Windows.Forms.TextBox TxtDia;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EjercicioPrg2/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioPrg2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Creamos las variables.
+        public decimal dia;
+        public string mes;
+        public decimal año;
+        public string Resultado;
+
+        //Constantes que luego saldran en los errores.
+        const string ERROR_DATOS = "Faltan datos de cargar.";
+        const string ERROR_DIAS = "Ingrese un dia valido.";
+        const string ERROR_NUMERICO = "Esta intentando ingresar una letra, ingrese un numero.";
+        const string ERROR_FEBRERO = "Febrero trae 28 dias.";
+
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+
+            if (!Error())
+            {
+                //Asignamos el valor a las variables.
+                dia = Convert.ToDecimal(TxtDia.Text);
+                mes = CmbBoxMes.Text;
+                año = Convert.ToDecimal(TxtAño.Text);
+
+                if (!ValidarDias())
+                {
+
+                    //Al dia que insertamos le sumamos 1+.
+                    if (0 < dia && dia < 30)
+                    {
+                        dia++;
+                    }
+                    
+                    if (mes == "2" && dia == 29)
+                    {
+                        dia = 28;
+                    }
+
+
+                    //Asignamos el valor en modo string a cada mes y dependiendo el dia que ingreso el usuario cambiara de mes o no.
+                    switch (mes)
+                    {
+                        case "1":
+                            mes = "Enero";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Febrero";
+                            }
+                            break;
+                        case "2":
+                            mes = "Febrero";
+                            if (dia == 28)
+                            {
+                                dia = 1;
+                                mes = "Marzo";
+                            }
+                            break;
+                        case "3":
+                            mes = "Marzo";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Abril";
+                            }
+                            break;
+                        case "4":
+                            mes = "Abril";
+                            if (dia == 30)
+                            {
+                                dia = 1;
+                                mes = "Mayo";
+                            }
+                            break;
+                        case "5":
+                            mes = "Mayo";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Junio";
+                            }
+                            break;
+                        case "6":
+                            mes = "Junio";
+                            if (dia == 30)
+                            {
+                                dia = 1;
+                                mes = "Julio";
+                            }
+                            break;
+                        case "7":
+                            mes = "Julio";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Agosto";
+                            }
+                            break;
+                        case "8":
+                            mes = "Agosto";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Septiembre";
+                            }
+                            break;
+                        case "9":
+                            mes = "Septiembre";
+                            if (dia == 30)
+                            {
+                                dia = 1;
+                                mes = "Octubre";
+                            }
+                            break;
+                        case "10":
+                            mes = "Octubre";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Noviembre";
+                            }
+                            break;
+                        case "11":
+                            mes = "Noviembre";
+                            if (dia == 30)
+                            {
+                                dia = 1;
+                                mes = "Diciembre";
+                            }
+                            break;
+                        case "12":
+                            mes = "Diciembre";
+                            if (dia == 31)
+                            {
+                                dia = 1;
+                                mes = "Enero";
+                                año++;
+                            }
+                            break;
+                        default:
+                            if (mes == "")
+                            {
+                                mes = "";
+                                break;
+                            }
+                            break;
+                    }
+                    LimpiarPantalla();
+
+                    //Damos el valor al label
+                    LblResultado.Text = dia.ToString() + " de " + mes + " de " + año.ToString();
+                }
+               
+            }
+
+        }
+
+        //Metodo para validar errores.
+        private bool Error()
+        {
+            bool bandera = false;
+
+            //Error por si el usuario ingresa letras.
+            for (int i = 0; i < TxtDia.Text.Length; i++)
+            {
+                char c = TxtDia.Text[i];
+
+                if (!char.IsDigit(c))
+                {
+                    bandera = true;
+                    MessageBox.Show(this, ERROR_NUMERICO, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+
+            //Error por si no se llena los casilleros.
+            if (!TxtDia.Text.Any() || !TxtAño.Text.Any() || !CmbBoxMes.Text.Any())
+            {
+                bandera = true;
+                MessageBox.Show(this, ERROR_DATOS, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return bandera;
+        }
+
+        //Metodo para validar dias.
+        public bool ValidarDias()
+        {
+            bool bandera = false;
+
+            if (1 > dia || dia > 31)
+            {
+                bandera = true;
+                MessageBox.Show(this, ERROR_DIAS, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (mes == "2" & 28 < dia)
+            {
+                bandera = true;
+                MessageBox.Show(this, ERROR_FEBRERO, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return bandera;
+        }
+
+        //Metodo para limpiar pantalla.
+        public void LimpiarPantalla()
+        {
+            TxtDia.Text = "";
+            TxtAño.Text = "";
+            CmbBoxMes.Text = "";
+            LblResultado.Text = "";
+        }
+    }
+
+}
+
+
